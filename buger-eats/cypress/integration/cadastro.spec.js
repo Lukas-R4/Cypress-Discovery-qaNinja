a9f2df9d3b68a2dc6f6851169ba803b3983b547a
Lukas-R4 <lucas93.rodrigues@gmail.com>
2021-12-26 18:24:57 -0300

    Refatoraçao (Aula 15)

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,50 +1,30 @@
 import 'cypress-file-upload';
+import signup from '../pages/SignupPage' // Já importa a classe instanciada
 
-describe ('Cadastro', ()=> {
+describe('Cadastro', () => {
 
-    it('Seja um entregador', ()=> {
-        cy.viewport(1920, 1080);
-        cy.visit('https://buger-eats.vercel.app');
+    beforeEach( function () {
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d
+        })
+    });
 
-        cy.get('a[href="/deliver"]').click();
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas');
+    it('Seja um entregador', function () {
 
-        
-        var entregador = {
-            nome: 'Lucas Rodrigues da Silva',
-            cpf: '13123455342',
-            email: 'lucas@teste.com',
-            whatsapp: '11999999999',
-            endereco: {
-                cep: '04534011',
-                rua: 'Rua Joaquim Floriano',
-                numero: '1000',
-                complemento: 'Ap 142',
-                bairro: 'Itaim Bibi',
-                cidade_uf: 'São Paulo/SP'
-            },
-            metodo_entrega: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
+        signup.go()
+        signup.fillForm(this.deliver.signup)
+        signup.submit()
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signup.modalContentShouldBe(expectedMessage)
+    })
 
-        cy.get('input[name="name"]').type(entregador.nome);
-        cy.get('input[name="cpf"]').type(entregador.cpf);
-        cy.get('input[name="email"]').type(entregador.email);
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+    it('CPF incorreto', function () {
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
+        signup.go()
+        signup.fillForm(this.deliver.cpf_inv)
+        signup.submit()
 
-        // Validar os campos preenchidos automaticamente
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua);
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro);
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf);
+        signup.alertMessageShouldBe('Oops! CPF inválido')
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click(); // O contains é uma função que permite usar o localizador + texto para encontrar um elemento
-
-        
-        cy.get('input[accept^="image"]').attachFile('/images/' + entregador.cnh);
     })
 })
